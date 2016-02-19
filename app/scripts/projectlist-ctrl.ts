11cb0684f3a9eb72f5a60b43651f8dca91944a2e
Eetu Mäkelä <eetu.makela@iki.fi>
2016-02-19 13:18:15 +0200

    add nuke option

--- app/scripts/projectlist-ctrl.ts
+++ app/scripts/projectlist-ctrl.ts
@@ -4,6 +4,7 @@ namespace fi.seco.recon {
   interface IProjectListScope extends angular.IScope {
     projects: ProjectInfo[]
     newProject: (projectId: string) => void
+    maybeDeleteAll: () => void
   }
 
   class ProjectInfo {
@@ -18,12 +19,24 @@ namespace fi.seco.recon {
 
   export class ProjectListController {
 
-    constructor($scope: IProjectListScope, $state: angular.ui.IStateService, $localStorage: IProjectStorage) {
+    constructor($scope: IProjectListScope,
+                $state: angular.ui.IStateService,
+                $localStorage: IProjectStorage,
+                $uibModal: angular.ui.bootstrap.IModalService) {
         if (!$localStorage.projects) $localStorage.projects = {}
         $scope.projects = []
         for (let project in $localStorage.projects)
           $scope.projects.push(new ProjectInfo(project, $localStorage.projects[project].state.data.length, $localStorage.projects[project].state.counts))
         $scope.newProject = (projectId: string) => { $state.go('project', {projectId: projectId})}
+        $scope.maybeDeleteAll = () => {
+          let modal: angular.ui.bootstrap.IModalServiceInstance = $uibModal.open({
+            templateUrl: 'deleteAll',
+          })
+          modal.result.then(() => {
+            $localStorage.projects = {}
+            $scope.projects = []
+          })
+        }
     }
   }
 }
